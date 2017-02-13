33135e4fb8f20ccbfb077d0f6df6567039f5a3c6
Kevin Schmidt <NSPtuning@gmail.com>
2017-02-13 12:44:05 -0800

    before lunch commit

--- app.js
+++ app.js
@@ -42,6 +42,32 @@ console.log(myUser);
 
 // console.log(myUser);
 
-myUser.login(); //runs from line 24 AFTER line 36
+// myUser.login(); //runs from line 24 AFTER line 36
+//
+// console.log('------------PRESENT ELEMENTS----------------');
+// // step 1 - create element node
+// var userElement = document.createElement('h1'); //window is the global scope so typing windows. is not needed
+// // userElement = (<h1></h1>)
+//
+// // step 2 - set element type - html element node - has all properties and element of element node
+// // ****SET ATTRIBUTES***
+// userElement.setAttribute('id', 'first-user-heading'); //takes 2 arguements
+// // userElement = <h1 id='first-user-heading'></h1>
+// // set all attributes before handing to DOM
+//
+// userElement.textContent = myUser.fullName;
+// // userElement = <h1 id="first-user-heading">sp,eIsema,e</h1>
+//
+// // step 3 - start
+// var sectionEl = document.getElementById('main-content');
+// // <section id="main-content"></section>
+//
+// // declare dom elements at the top of the page.
+// // var userInputEl = getElement('main-content');
+// // step 3 send to DOM
+// // add child to element
+// sectionEl.appendChild(userElement); //section tag onindex.html
 
-console.log('------------PRESENT TWEETS----------------');
+//approach #2 ------------------------------------------
+var userHeadingEl = document.getElementById('first-user-heading');
+userHeadingEl.textContent = myUser.fullName;
